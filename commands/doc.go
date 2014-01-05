06ff7caa074b7749d22bd5cef1f91c373a66b662
Burcu Dogan <burcujdogan@gmail.com>
2014-01-05 21:57:05 +0100

    Add license and some docs.

--- commands/doc.go
+++ commands/doc.go
@@ -12,29 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package provides commands to run load tests and display results.
 package commands
-
-import (
-	"net/http"
-	"time"
-
-	"github.com/cheggaaa/pb"
-)
-
-type result struct {
-	err        error
-	statusCode int
-	duration   time.Duration
-}
-
-type Boom struct {
-	Req    *http.Request
-	N      int
-	C      int
-	Client *http.Client
-
-	start   time.Time
-	end     time.Time
-	results chan *result
-	bar     *pb.ProgressBar
-}
