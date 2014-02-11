e47d49c8cf6af9dc949433951e5beac44cd0f38a
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-02-10 22:31:57 -0600

    Added support for TCP/HTTP proxy

--- commands/run.go
+++ commands/run.go
@@ -16,6 +16,7 @@ package commands
 
 import (
 	"crypto/tls"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"sync"
@@ -36,6 +37,11 @@ func (b *Boom) worker(ch chan *http.Request) {
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: b.AllowInsecure, ServerName: host},
 	}
+	if b.ProxyAddr != "" {
+		tr.Dial = func(network string, addr string) (conn net.Conn, err error) {
+			return net.Dial(network, b.ProxyAddr)
+		}
+	}
 	client := &http.Client{Transport: tr}
 	for req := range ch {
 		s := time.Now()
@@ -47,6 +53,8 @@ func (b *Boom) worker(ch chan *http.Request) {
 			if resp.ContentLength > 0 {
 				size = resp.ContentLength
 			}
+			// consume the whole body
+			ioutil.ReadAll(resp.Body)
 			// cleanup body, so the socket can be reusable
 			resp.Body.Close()
 		}
