4111a6d5a138d3ebc40b1fcb739c060f1dd9b679
Burcu Dogan <jbd@google.com>
2015-11-11 11:12:06 -0800

    Reorganize boomer.

--- boomer/boomer.go
+++ boomer/boomer.go
@@ -16,9 +16,13 @@
 package boomer
 
 import (
+	"crypto/tls"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/rakyll/pb"
@@ -94,4 +98,95 @@ func newPb(size int) (bar *pb.ProgressBar) {
 	bar.Format("Bom !")
 	bar.Start()
 	return
+}
+
+// client is the http.Client that will be used to make all requests
+// to the destination.
+var client *http.Client
+
+// Run makes all the requests, prints the summary. It blocks until
+// all work is done.
+func (b *Boomer) Run() {
+	b.results = make(chan *result, b.N)
+	if b.Output == "" {
+		b.bar = newPb(b.N)
+	}
+
+	start := time.Now()
+	b.run()
+	if b.Output == "" {
+		b.bar.Finish()
+	}
+
+	printReport(b.N, b.results, b.Output, time.Now().Sub(start))
+	close(b.results)
+}
+
+func (b *Boomer) worker(wg *sync.WaitGroup, ch chan *http.Request, readAll bool) {
+	for req := range ch {
+		s := time.Now()
+
+		var code int
+		var size int64
+
+		resp, err := client.Do(req)
+
+		if err == nil {
+			size = resp.ContentLength
+			code = resp.StatusCode
+			if readAll {
+				_, err = io.Copy(ioutil.Discard, resp.Body)
+			}
+			resp.Body.Close()
+		}
+
+		if b.bar != nil {
+			b.bar.Increment()
+		}
+
+		wg.Done()
+		b.results <- &result{
+			statusCode:    code,
+			duration:      time.Now().Sub(s),
+			err:           err,
+			contentLength: size,
+		}
+	}
+}
+
+func (b *Boomer) run() {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: b.AllowInsecure,
+		},
+		DisableCompression: b.DisableCompression,
+		DisableKeepAlives:  b.DisableKeepAlives,
+		// TODO(jbd): Add dial timeout.
+		TLSHandshakeTimeout: time.Duration(b.Timeout) * time.Millisecond,
+		Proxy:               http.ProxyURL(b.ProxyAddr),
+	}
+	client = &http.Client{Transport: tr}
+
+	var wg sync.WaitGroup
+	wg.Add(b.N)
+
+	var throttle <-chan time.Time
+	if b.Qps > 0 {
+		throttle = time.Tick(time.Duration(1e6/(b.Qps)) * time.Microsecond)
+	}
+	jobs := make(chan *http.Request, b.N)
+	for i := 0; i < b.C; i++ {
+		go func() {
+			b.worker(&wg, jobs, b.Req.ReadAll)
+		}()
+	}
+	for i := 0; i < b.N; i++ {
+		if b.Qps > 0 {
+			<-throttle
+		}
+		jobs <- b.Req.Request()
+	}
+	close(jobs)
+
+	wg.Wait()
 }
