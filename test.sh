94e8859a18abf86c0197297f641f34d162c5ab6d
Vincent Thiery <vjmthiery@gmail.com>
2017-12-29 22:46:32 +0100

    Add coverage report generation script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+echo "" > coverage.out
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.out
+        rm profile.out
+    fi
+done
