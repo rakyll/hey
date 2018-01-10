e4f1f9bac4752c750f3c84d7fbd3632eced2b5b5
Gil Raphaelli <g@raphaelli.com>
2018-01-10 15:21:11 -0500

    expose error and status code dists

--- requester/requester.go
+++ requester/requester.go
@@ -265,3 +265,11 @@ func min(a, b int) int {
 	}
 	return b
 }
+
+func (w *Work) ErrorDist() map[string]int {
+	return w.report.errorDist
+}
+
+func (w *Work) StatusCodes() map[int]int {
+	return w.report.statusCodeDist
+}
