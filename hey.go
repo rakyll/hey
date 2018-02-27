8c8041818b04ad7f3edcfc1e2ab8aef0dffd9df1
Pengcheng Chen <pengcheng.chen@gmail.com>
2018-02-27 11:22:18 -0800

    Fixed basic auth

--- hey.go
+++ hey.go
@@ -198,9 +198,6 @@ func main() {
 		usageAndExit(err.Error())
 	}
 	req.ContentLength = int64(len(bodyAll))
-	if username != "" || password != "" {
-		req.SetBasicAuth(username, password)
-	}
 
 	// set host header if set
 	if *hostHeader != "" {
@@ -215,6 +212,11 @@ func main() {
 	}
 	header.Set("User-Agent", ua)
 	req.Header = header
+
+	if username != "" || password != "" {
+		req.SetBasicAuth(username, password)
+		fmt.Printf("username %s, password %s\n", username, password)
+	}
 
 	w := &requester.Work{
 		Request:            req,
