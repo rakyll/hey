81437ada3aca91f43b5fb2202b9de4a3a00386fd
Sergey Soldatov <ssacompany@mail.ru>
2020-03-24 21:48:29 +0300

    update doc

--- hey.go
+++ hey.go
@@ -102,13 +102,16 @@ Options:
   -cpus                 Number of used cpu cores.
                         (default for current machine is %d cores)
 
-  -enable-tmpl  Header or body may include templates for random requests.
+  -enable-tmpl  Header or body may include templates for generating random requests.
                 Allowed {{.Email}}, {{.RequestID}}, {{.Time}}, {{.DtTm}},
                 {{.Date}}, {{.Integer}}, {{.Float}}, {{.String}}.
                 May be used multiple times in header or body.
                 Ex1: -H 'x-request-id: {{.RequestID}}'
+                reived x-request-id: 920bdca1fd9546f49cf4d9a0d1117cc8
                 Ex2: -d '{ "user_email": "{{.Email}}", "user_login": "{{.String}}" }'
+                recived { "user_email": " tghr94rql8@i3pj3.com", "user_login": "ee2r2s8qpt" }
                 Ex3: -d '{ "primary_email": "{{.Email_1}}", "secondary_email": "{{.Email_2}}" }'
+                recived { "primary_email": "8l928nbeiu@zhdpp.com", "secondary_email": "all01bcj4l@3s330.com" }
 `
 
 func main() {
