2872e12867e1d90b7fbbd6e26d20ea7253994ef5
Jitendra A <2908547+adhocore@users.noreply.github.com>
2023-01-28 20:14:41 +0700

    Test dynamic request generator

---
+++ requester/generator_test.go
@@ -0,0 +1,125 @@
+package requester
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+)
+
+type testVal struct {
+	Int  int     `json:"int"`
+	Flt  float64 `json:"flt"`
+	Str  string  `json:"str"`
+	Int1 int     `json:"int1"`
+	Flt1 float64 `json:"flt1"`
+	Str1 string  `json:"str1"`
+}
+
+func TestRequestGenerator(t *testing.T) {
+	req, _ := http.NewRequest("POST", "http://localhost", nil)
+	t.Run("static", func(t *testing.T) {
+		body := []byte(`{"static":"content"}`)
+		if RequestGenerator(req, body) != nil {
+			t.Errorf("request generator must give nil for static body")
+		}
+	})
+
+	t.Run("int", func(t *testing.T) {
+		body := []byte(`{"int":{i:1:100}}`)
+		fn := RequestGenerator(req, body)
+		if fn == nil {
+			t.Errorf("request generator must give generator func for dynamic body with placeholder")
+		}
+		newBody, _ := io.ReadAll(fn().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if val.Int < 1 || val.Int > 100 {
+			t.Errorf("i:1:100 must give integer between 1 and 100")
+		}
+	})
+
+	t.Run("float", func(t *testing.T) {
+		body := []byte(`{"flt":{f:10:20}}`)
+		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if val.Flt < 10 || val.Flt > 20 {
+			t.Errorf("f:10:20 must give float between 10 and 20")
+		}
+	})
+
+	t.Run("string", func(t *testing.T) {
+		body := []byte(`{"str":"{s:5:10}"}`)
+		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if len(val.Str) < 5 || len(val.Str) > 10 {
+			t.Errorf("s:5:10 must give string with 5 to 10 chars")
+		}
+	})
+
+	t.Run("mix", func(t *testing.T) {
+		body := []byte(`{"int":{i:1:100},"flt":{f:10:20},"str":"{s:5:10}"}`)
+		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if val.Int < 1 || val.Int > 100 {
+			t.Errorf("i:1:100 must give integer between 1 and 100")
+		}
+		if val.Flt < 10 || val.Flt > 20 {
+			t.Errorf("f:10:20 must give float between 10 and 20")
+		}
+		if len(val.Str) < 5 || len(val.Str) > 10 {
+			t.Errorf("s:5:10 must give string with 5 to 10 chars")
+		}
+	})
+
+	t.Run("repeat", func(t *testing.T) {
+		body := []byte(`{"int":{i:1:100},"int1": {i:100:1000}, "flt":{f:20:50},"flt1":{f}, "str":"{s:25:100}","str1":"{s}"}`)
+		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if val.Int != val.Int1 {
+			t.Errorf("same placeholder name i must give same integer")
+		}
+		if val.Flt != val.Flt1 {
+			t.Errorf("same placeholder name f must give same float")
+		}
+		if val.Str != val.Str1 {
+			t.Errorf("same placeholder name s must give same string")
+		}
+	})
+
+	t.Run("n-repeat", func(t *testing.T) {
+		body := []byte(`{"int":{i2:10:500},"int1": {i2}, "flt":{f3:100:2000},"flt1":{f3}, "str":"{s4:50:100}","str1":"{s4}"}`)
+		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if val.Int != val.Int1 {
+			t.Errorf("same placeholder name i2 must give same integer")
+		}
+		if val.Flt != val.Flt1 {
+			t.Errorf("same placeholder name f3 must give same float")
+		}
+		if val.Str != val.Str1 {
+			t.Errorf("same placeholder name s4 must give same string")
+		}
+	})
+
+	t.Run("no-min:max", func(t *testing.T) {
+		body := []byte(`{"int":{i}, "flt":{f}, "str":"{s}"}`)
+		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if val.Int < 1 || val.Int > 10 {
+			t.Errorf("i must give integer between 1 and 10")
+		}
+		if val.Flt < 1 || val.Flt > 10 {
+			t.Errorf("f must give float between 1 and 10")
+		}
+		if len(val.Str) < 1 || len(val.Str) > 10 {
+			t.Errorf("s must give string with 1 to 10 chars")
+		}
+	})
+}
