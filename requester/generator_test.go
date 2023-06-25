c25b0aba38605868016e84c08dd6d42275ca2e43
Jitendra A <2908547+adhocore@users.noreply.github.com>
2023-06-25 22:08:57 +0700

    Test dynamic uri path, params and all in one

--- requester/generator_test.go
+++ requester/generator_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"regexp"
+	"strings"
 	"testing"
 )
 
@@ -17,19 +19,28 @@ type testVal struct {
 }
 
 func TestRequestGenerator(t *testing.T) {
-	req, _ := http.NewRequest("POST", "http://localhost", nil)
 	t.Run("static", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
 		body := []byte(`{"static":"content"}`)
 		if RequestGenerator(req, body) != nil {
 			t.Errorf("request generator must give nil for static body")
 		}
 	})
 
+	t.Run("url path/param", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "http://localhost/{s:1:1}/{s1:2:2}?page={i:1:9}&per_page={i1:10:99}&filter={f:1:9}", nil)
+		uri := strings.Replace(RequestGenerator(req, []byte{})().URL.String(), "%20", " ", -1)
+		if match, _ := regexp.MatchString(`http://localhost/./..\?page=\d&per_page=\d\d&filter=\d\.\d\d$`, uri); !match {
+			t.Errorf("dynamic uri path/param must be interpolated: %s", uri)
+		}
+	})
+
 	t.Run("int", func(t *testing.T) {
-		body := []byte(`{"int":{i:1:100}}`)
-		fn := RequestGenerator(req, body)
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
+		fn := RequestGenerator(req, []byte(`{"int":{i:1:100}}`))
 		if fn == nil {
 			t.Errorf("request generator must give generator func for dynamic body with placeholder")
+			return
 		}
 		newBody, _ := io.ReadAll(fn().Body)
 		val := testVal{}
@@ -40,8 +51,8 @@ func TestRequestGenerator(t *testing.T) {
 	})
 
 	t.Run("float", func(t *testing.T) {
-		body := []byte(`{"flt":{f:10:20}}`)
-		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
+		newBody, _ := io.ReadAll(RequestGenerator(req, []byte(`{"flt":{f:10:20}}`))().Body)
 		val := testVal{}
 		json.Unmarshal(newBody, &val)
 		if val.Flt < 10 || val.Flt > 20 {
@@ -50,8 +61,8 @@ func TestRequestGenerator(t *testing.T) {
 	})
 
 	t.Run("string", func(t *testing.T) {
-		body := []byte(`{"str":"{s:5:10}"}`)
-		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
+		newBody, _ := io.ReadAll(RequestGenerator(req, []byte(`{"str":"{s:5:10}"}`))().Body)
 		val := testVal{}
 		json.Unmarshal(newBody, &val)
 		if len(val.Str) < 5 || len(val.Str) > 10 {
@@ -60,6 +71,7 @@ func TestRequestGenerator(t *testing.T) {
 	})
 
 	t.Run("mix", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
 		body := []byte(`{"int":{i:1:100},"flt":{f:10:20},"str":"{s:5:10}"}`)
 		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
 		val := testVal{}
@@ -76,6 +88,7 @@ func TestRequestGenerator(t *testing.T) {
 	})
 
 	t.Run("repeat", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
 		body := []byte(`{"int":{i:1:100},"int1": {i:100:1000}, "flt":{f:20:50},"flt1":{f}, "str":"{s:25:100}","str1":"{s}"}`)
 		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
 		val := testVal{}
@@ -92,6 +105,7 @@ func TestRequestGenerator(t *testing.T) {
 	})
 
 	t.Run("n-repeat", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
 		body := []byte(`{"int":{i2:10:500},"int1": {i2}, "flt":{f3:100:2000},"flt1":{f3}, "str":"{s4:50:100}","str1":"{s4}"}`)
 		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
 		val := testVal{}
@@ -108,6 +122,7 @@ func TestRequestGenerator(t *testing.T) {
 	})
 
 	t.Run("no-min:max", func(t *testing.T) {
+		req, _ := http.NewRequest("POST", "http://localhost", nil)
 		body := []byte(`{"int":{i}, "flt":{f}, "str":"{s}"}`)
 		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
 		val := testVal{}
@@ -120,6 +135,21 @@ func TestRequestGenerator(t *testing.T) {
 		}
 		if len(val.Str) < 1 || len(val.Str) > 10 {
 			t.Errorf("s must give string with 1 to 10 chars")
+		}
+	})
+
+	t.Run("url path/param request body", func(t *testing.T) {
+		body := []byte(`{"int":{i}, "flt":{f}, "str":"{s}"}`)
+		req, _ := http.NewRequest("POST", "http://localhost/{i9:1:9}/{f9:10:99}?page={i10:1:9}", nil)
+		nreq := RequestGenerator(req, body)()
+		if match, _ := regexp.MatchString(`/\d/\d\d\.\d\d\?page=\d$`, nreq.URL.String()); !match {
+			t.Errorf("dynamic uri path must be interpolated: %s", nreq.URL.String())
+		}
+		newBody, _ := io.ReadAll(RequestGenerator(req, body)().Body)
+		val := testVal{}
+		json.Unmarshal(newBody, &val)
+		if val.Int < 1 || val.Int > 10 {
+			t.Errorf("i must give integer between 1 and 10")
 		}
 	})
 }
